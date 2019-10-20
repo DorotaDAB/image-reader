9d7178ea9dfb50bdcdd142a69c1e24b38579c6dd
DorotaDAB <dorota.dabrowska.kowalska@gmail.com>
2019-10-20 14:23:51 +0200

    Updates

---
+++ precache-manifest.c154813507bc1bd7f4f9bf3c01bb0f29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593491d5be1471f40436366f58e2227f",
+    "url": "/image-reader/index.html"
+  },
+  {
+    "revision": "05b141a08316ea2aa39d",
+    "url": "/image-reader/static/css/main.652c2684.chunk.css"
+  },
+  {
+    "revision": "78eb3e8ee263413794cc",
+    "url": "/image-reader/static/js/2.80f7f3b2.chunk.js"
+  },
+  {
+    "revision": "05b141a08316ea2aa39d",
+    "url": "/image-reader/static/js/main.30ced85f.chunk.js"
+  },
+  {
+    "revision": "5b41b78c49efc22a47cb",
+    "url": "/image-reader/static/js/runtime-main.9d0aabaa.js"
+  }
+]);
